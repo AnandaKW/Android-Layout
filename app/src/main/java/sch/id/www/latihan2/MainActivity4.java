6c908295aa0ca84da1d03d13249c8e6689a31ee9
AnandaKW <anandadinar765@gmail.com>
2022-02-18 10:52:41 +0700

    First Commit

---
+++ app/src/main/java/sch/id/www/latihan2/MainActivity4.java
@@ -0,0 +1,14 @@
+package sch.id.www.latihan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
